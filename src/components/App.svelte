5a7817ae3ca8dcfcf774a8c2192ebcec184eabd0
Ciro Zhang <129805733+CiroZhang@users.noreply.github.com>
2024-05-28 20:41:46 -0700

    Update App.svelte

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -20,7 +20,7 @@
   const bfsPath = writable([]);
 
   async function loadGraphData() {
-    const data = await d3.csv('wikipedia-game-bfs/src/components/wiki.csv');
+    const data = await d3.csv('./wiki.csv');
     const parsedNodes = new Set();
     const parsedLinks = [];
 
